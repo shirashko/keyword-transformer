a0a1ba927ca9d19cbc0f6f5db257b8f413f279a4
galgetz1 <galge@wix.com>
2026-03-23 20:29:59 +0200

    Remove unused layers, upstream docs, and clean up model references
    - Delete 12 unused layer files (stream, svdf, lstm, gru, delay, etc.)
    - Delete 17 broken test files for removed layers
    - Delete upstream docs: README.md, requirements.txt, eval.sh
    - Trim experiment files to only keep att_mh_rnn sections
    - Clean model_params.py: keep only Params class and att_mh_rnn_params
    - Clean utils.py: remove get_model_with_default_params and model_params import
    - Clean models_test.py and utils_test.py: remove references to deleted models

--- kws_streaming/experiments/kws_experiments_12_labels.md
+++ kws_streaming/experiments/kws_experiments_12_labels.md
@@ -71,63 +71,12 @@ mkdir $KWS_PATH/models_data_v2_12_labels
 MODELS_PATH=$KWS_PATH/models_data_v2_12_labels
 ```
 
-After all of these, main folder KWS_PATH should have several subfolders:
-```
-  kws_streaming/
-    colab/
-    data/
-    experiments/
-    ...
-  data2
-    _background_noise_/
-    bed/
-    ...
-  models_data_v2_12_labels/
-    ...
-```
-
 ## Models training and evaluation:
 
 There are two options of running python script. One with bazel and another by calling python directly shown below:
 ```shell
 # CMD_TRAIN="bazel run -c opt --copt=-mavx2 kws_streaming/train:model_train_eval --"
 CMD_TRAIN="python -m kws_streaming.train.model_train_eval"
-```
-
-### ds_tc_resnet - based on [MatchboxNet](https://arxiv.org/pdf/2004.08531.pdf)
-
-parameters: 75K \
-float accuracy 98.0 \
-```shell
-$CMD_TRAIN \
---data_url '' \
---data_dir $DATA_PATH/ \
---train_dir $MODELS_PATH/ds_tc_resnet/ \
---mel_upper_edge_hertz 7600 \
---how_many_training_steps 40000,40000,20000,20000 \
---learning_rate 0.001,0.0005,0.0002,0.0001 \
---window_size_ms 30.0 \
---window_stride_ms 10.0 \
---mel_num_bins 80 \
---dct_num_features 40 \
---resample 0.15 \
---alsologtostderr \
---train 1 \
---use_spec_augment 1 \
---time_masks_number 2 \
---time_mask_max_size 25 \
---frequency_masks_number 2 \
---frequency_mask_max_size 7 \
---pick_deterministically 1 \
-ds_tc_resnet \
---activation 'relu' \
---dropout 0.0 \
---ds_filters '128, 64, 64, 64, 128, 128' \
---ds_repeat '1, 1, 1, 1, 1, 1' \
---ds_residual '0, 1, 1, 1, 0, 0' \
---ds_kernel_size '11, 13, 15, 17, 29, 1' \
---ds_stride '1, 1, 1, 1, 1, 1' \
---ds_dilation '1, 1, 1, 1, 2, 1'
 ```
 
 ### att_mh_rnn
--- kws_streaming/models/utils_test.py
+++ kws_streaming/models/utils_test.py
@@ -15,12 +15,10 @@
 
 """Tests for kws_streaming.models.utils."""
 
-from absl import flags
 from absl.testing import parameterized
 import numpy as np
 
 from kws_streaming.layers import modes
-from kws_streaming.layers import test_utils
 from kws_streaming.layers.compat import tf
 from kws_streaming.layers.compat import tf1
 from kws_streaming.models import model_flags
@@ -28,8 +26,6 @@ from kws_streaming.models import model_params
 from kws_streaming.models import models
 from kws_streaming.models import utils
 tf1.disable_eager_execution()
-
-FLAGS = flags.FLAGS
 
 
 class SequentialModel(tf.keras.Model):
@@ -65,7 +61,7 @@ class UtilsTest(tf.test.TestCase, parameterized.TestCase):
   def _testTFLite(self,
                   preprocess='raw',
                   feature_type='mfcc_op',
-                  model_name='svdf'):
+                  model_name='att_mh_rnn'):
     params = model_params.HOTWORD_MODEL_PARAMS[model_name]
     params.clip_duration_ms = 100  # make it shorter for testing
 
@@ -115,110 +111,23 @@ class UtilsTest(tf.test.TestCase, parameterized.TestCase):
   def testPreprocessNonStreamInferenceTFandTFLite(self,
                                                   preprocess,
                                                   feature_type,
-                                                  model_name='svdf'):
+                                                  model_name='att_mh_rnn'):
     # Validate that model with different preprocessing
     # can be converted to non stream inference mode.
     self._testTFLite(preprocess, feature_type, model_name)
 
-  @parameterized.named_parameters([
-      {
-          'testcase_name': 'raw with mfcc_tf',
-          'preprocess': 'raw',
-          'feature_type': 'mfcc_tf'
-      },
-      {
-          'testcase_name': 'raw with mfcc_op',
-          'preprocess': 'raw',
-          'feature_type': 'mfcc_op'
-      },
-      {
-          'testcase_name': 'mfcc',
-          'preprocess': 'mfcc',
-          'feature_type': 'mfcc_op'
-      },  # feature_type will be ignored
-      {
-          'testcase_name': 'micro',
-          'preprocess': 'micro',
-          'feature_type': 'mfcc_op'
-      },  # feature_type will be ignored
-  ])
-  def testPreprocessStreamInferenceModeTFandTFLite(self,
-                                                   preprocess,
-                                                   feature_type,
-                                                   model_name='gru'):
-    # Validate that model with different preprocessing
-    # can be converted to stream inference mode with TF and TFLite.
-    params = model_params.HOTWORD_MODEL_PARAMS[model_name]
-    # set parameters to test
-    params.preprocess = preprocess
-    params.feature_type = feature_type
-    params = model_flags.update_flags(params)
-
-    # create model
-    model = models.MODELS[params.model_name](params)
-
-    # convert TF non streaming model to TFLite streaming inference
-    # with external states
-    self.assertTrue(
-        utils.model_to_tflite(self.sess, model, params,
-                              modes.Modes.STREAM_EXTERNAL_STATE_INFERENCE))
-
-    # convert TF non streaming model to TF streaming with external states
-    self.assertTrue(
-        utils.to_streaming_inference(
-            model, params, modes.Modes.STREAM_EXTERNAL_STATE_INFERENCE))
-
-    # convert TF non streaming model to TF streaming with internal states
-    self.assertTrue(
-        utils.to_streaming_inference(
-            model, params, modes.Modes.STREAM_INTERNAL_STATE_INFERENCE))
-
-  def test_model_to_saved(self, model_name='dnn'):
-    """SavedModel supports both stateless and stateful graphs."""
-    params = model_params.HOTWORD_MODEL_PARAMS[model_name]
-    params = model_flags.update_flags(params)
-
-    # create model
-    model = models.MODELS[params.model_name](params)
-    utils.model_to_saved(model, params, FLAGS.test_tmpdir)
-
   def testNextPowerOfTwo(self):
     self.assertEqual(utils.next_power_of_two(11), 16)
 
-  @parameterized.parameters('att_mh_rnn', 'att_rnn', 'dnn', 'ds_cnn', 'cnn',
-                            'tc_resnet', 'crnn', 'gru', 'lstm', 'svdf',
-                            'mobilenet', 'mobilenet_v2', 'xception',
-                            'inception', 'inception_resnet', 'ds_tc_resnet')
-  def testNonStreamInferenceTFandTFLite(self, model_name):
-    # Validate that all models with selected preprocessing
+  def testNonStreamInferenceTFandTFLite_att_mh_rnn(self):
+    # Validate att_mh_rnn with selected preprocessing
     # can be converted to non stream inference mode.
-    self._testTFLite(model_name=model_name)
-
-  @parameterized.parameters(
-      'cnn_stride',
-      'cnn',
-      'crnn',
-      'dnn',
-      'ds_tc_resnet',
-      'gru',
-      'lstm',
-      'svdf',
-  )
-  def test_external_streaming_shapes(self, model_name):
-    model = utils.get_model_with_default_params(
-        model_name, mode=modes.Modes.STREAM_EXTERNAL_STATE_INFERENCE)
-
-    # The first 'n' inputs correspond to the 'n' inputs that the model takes
-    # in non-streaming mode. The rest of the input tensors represent the
-    # internal states for each layer in the model.
-    inputs = [np.zeros(shape, dtype=np.float32) for shape in model.input_shapes]
-    outputs = model.predict(inputs)
-    for output, expected_shape in zip(outputs, model.output_shapes):
-      self.assertEqual(output.shape, expected_shape)
+    self._testTFLite(model_name='att_mh_rnn')
 
   def test_sequential_to_functional(self):
     # prepare input data
-    test_utils.set_seed(1)
+    np.random.seed(1)
+    tf.random.set_seed(1)
     batch_input_shape = (1, 4, 2, 2)
     input_data = np.random.rand(np.prod(batch_input_shape))
     input_data = np.reshape(input_data, batch_input_shape)
